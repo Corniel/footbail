828e7c2811757880e88434dbb669697b53fbf3aa
Corniel Nobel <mail@corniel.nl>
2020-03-15 19:32:34 +0100

    Added football pitch..

---
+++ src/Footbail/FootballPitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace Footbail
+{
+    /// <summary>Represents the football pitch.</summary>
+    public class FootballPitch
+    {
+        public FootballPitch(double touchLine, double goalLine)
+        {
+            TouchLine = Guard.IsNumber(touchLine, nameof(touchLine));
+            GoalLine = Guard.IsNumber(goalLine, nameof(goalLine));
+        }
+
+        /// <summary>Gets the touch line size (width) of the football pitch.</summary>
+        public double TouchLine { get; }
+
+        /// <summary>Gets the goal line size (height) of the football pitch.</summary>
+        public double GoalLine { get; }
+
+        /// <summary>Returns true if the position is out-of-play.</summary>
+        public bool OutOfPlay(Position position)
+        {
+            var x = position.X;
+            var y = position.Y;
+
+            return x < MinX || x > MaxX
+                || y < MinY || y > MaxY;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "Touch line: {0}, Goal line: {1}", TouchLine, GoalLine);
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private double MinX => -TouchLine / 2;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private double MaxX => TouchLine / 2;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private double MinY => -GoalLine / 2;
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private double MaxY => GoalLine / 2;
+    }
+}
---
+++ test/Footbail.UnitTests/FootballPitchTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Footbail.UnitTests
+{
+    public class FootballPitchTest
+    {
+        [TestCase(false, "(15, 4)", "not out-of-play")]
+        [TestCase(true, "(15, -34.1)", "Below")]
+        [TestCase(true, "(15, +34.1)", "Above")]
+        [TestCase(true, "(-53, +33)", "Left")]
+        [TestCase(true, "(+53, +33)", "Right")]
+        public void OutOfPlay(bool expected, Position position, string message)
+        {
+            var pitch = new FootballPitch(touchLine: 105, goalLine: 68);
+            var outOfPlay = pitch.OutOfPlay(position);
+            Assert.AreEqual(expected, outOfPlay, message);
+        }
+    }
+}
