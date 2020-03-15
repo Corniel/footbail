eaa8b10c561f16ddc78b68e532bfc7cabe4dc92a
Corniel Nobel <mail@corniel.nl>
2020-03-15 19:37:54 +0100

    Initial setup of the rules.

--- src/Footbail/FootballPitch.cs
+++ src/Footbail/FootballPitch.cs
@@ -6,10 +6,14 @@ namespace Footbail
     /// <summary>Represents the football pitch.</summary>
     public class FootballPitch
     {
-        public FootballPitch(double touchLine, double goalLine)
+        public FootballPitch(
+            double touchLine, 
+            double goalLine,
+            bool bouncingLines = false)
         {
             TouchLine = Guard.IsNumber(touchLine, nameof(touchLine));
             GoalLine = Guard.IsNumber(goalLine, nameof(goalLine));
+            BouncingLines = bouncingLines;
         }
 
         /// <summary>Gets the touch line size (width) of the football pitch.</summary>
@@ -17,6 +21,9 @@ namespace Footbail
 
         /// <summary>Gets the goal line size (height) of the football pitch.</summary>
         public double GoalLine { get; }
+
+        /// <summary>Indicates if the lines bounce (or not).</summary>
+        public bool BouncingLines { get; }
 
         /// <summary>Returns true if the position is out-of-play.</summary>
         public bool OutOfPlay(Position position)
@@ -31,7 +38,13 @@ namespace Footbail
         /// <inheritdoc/>
         public override string ToString()
         {
-            return string.Format(CultureInfo.InvariantCulture, "Touch line: {0}, Goal line: {1}", TouchLine, GoalLine);
+            return string.Format(
+                CultureInfo.InvariantCulture, 
+                "Touch line: {0}, Goal line: {1}{2}",
+                TouchLine,
+                GoalLine,
+                BouncingLines ? ", Bouncing lines" : ""
+            );
         }
 
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
---
+++ src/Footbail/Rules.cs
@@ -0,0 +1,20 @@
+﻿namespace Footbail
+{
+    public class Rules
+    {
+        public FootballPitch FootballPitch { get; set; }
+        public int PlayerCount { get; set; }
+
+        public bool WithOffside { get; set; }
+        public bool WithKeeper { get; set; }
+        public int Turns { get; set; } = 45 * 2 * 10;
+
+
+        public double BallAccelaration { get; set; } = 0.99;
+
+        public double OldMoveNewMoveRatio { get; set; } = 0.75;
+        public double NewMoveOldMoveRation => 1 - OldMoveNewMoveRatio;
+
+        public double ShootSlowdownFactor { get; set; } = 0.5;
+    }
+}
