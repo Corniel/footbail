bc86315032bdde846455c5b7134080fd04fb3707
Corniel Nobel <mail@corniel.nl>
2020-03-15 15:44:13 +0100

    The actions to apply.

---
+++ src/Footbail/Actions/Shoot.cs
@@ -0,0 +1,23 @@
+﻿namespace Footbail.Actions
+{
+    /// <summary>Represents a shoot action.</summary>
+    public class Shoot
+    {
+        /// <summary>Creates a new instance of the <see cref="Shoot"/> class.</summary>
+        public Shoot(Team team, int number, Velocity velocity)
+        {
+            Team = team;
+            Number = number;
+            Velocity = velocity;
+        }
+
+        /// <summary>Gets the team of the player to act.</summary>
+        public Team Team { get; }
+
+        /// <summary>Gets the number of the player to act.</summary>
+        public int Number { get; }
+
+        /// <summary>Gets the velocity to shoot with.</summary>
+        public Velocity Velocity { get; set; }
+    }
+}
